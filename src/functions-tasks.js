359f2ed77c15e0854d10f9fce82c7aaa23ab3d02
dnemirich <dnemirich@gmail.com>
2025-04-10 02:11:20 +0300

    feat: 6/11 tasks solved

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -118,8 +118,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+  return function memo() {
+    if (!cache) {
+      cache = func();
+    }
+    return cache;
+  };
 }
 
 /**
